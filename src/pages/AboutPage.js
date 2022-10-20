aa550dafb1bd8c1d34beb3b40fc65dd087f249c5
Filip <filip.trifunovic23@gmail.com>
2022-10-20 23:41:56 +0200

    sidebar toggle, error page, about, checkout

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,7 +1,57 @@
 import React from "react";
+import styled from "styled-components";
+import { PageHero } from "../components";
+import aboutImg from "../assets/hero-bcg.jpeg";
 
-function AboutPage() {
-  return <div>AboutPage</div>;
-}
+const AboutPage = () => {
+  return (
+    <main>
+      <PageHero title="about" />
+      <Wrapper className="page section section-center">
+        <img src={aboutImg} alt="nice desk" />
+        <article>
+          <div className="title">
+            <h2>our story</h2>
+            <div className="underline"></div>
+          </div>
+          <p>
+            Modern home design emphasizes clean lines and geometric shapes. No
+            more are features such as arches, ornate columns, window shutters,
+            or any outlandish ornamentation. In place of these features that at
+            one point spoke of luxury and wealth are simple shapes and
+            intentional asymmetry. The opulence of the past is no more.
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
 
+const Wrapper = styled.section`
+  display: grid;
+  gap: 4rem;
+  img {
+    width: 100%;
+    display: block;
+    border-radius: var(--radius);
+    height: 500px;
+    object-fit: cover;
+  }
+  p {
+    line-height: 2;
+    max-width: 45em;
+    margin: 0 auto;
+    margin-top: 2rem;
+    color: var(--clr-grey-5);
+  }
+  .title {
+    text-align: left;
+  }
+  .underline {
+    margin-left: 0;
+  }
+  @media (min-width: 992px) {
+    grid-template-columns: 1fr 1fr;
+  }
+`;
 export default AboutPage;
--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,7 +1,33 @@
 import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+const ErrorPage = () => {
+  return (
+    <Wrapper className="page-100">
+      <section>
+        <h1>404</h1>
+        <h3>Sorry, there is no page.</h3>
+        <Link to="/" className="btn">
+          Back home
+        </Link>
+      </section>
+    </Wrapper>
+  );
+};
 
-function ErrorPage() {
-  return <div>ErrorPage</div>;
-}
+const Wrapper = styled.main`
+  background: var(--clr-primary-10);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  h1 {
+    font-size: 10rem;
+  }
+  h3 {
+    text-transform: none;
+    margin-bottom: 2rem;
+  }
+`;
 
 export default ErrorPage;
