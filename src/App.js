65efe586907c0794b56a86dd40f2c732c6d4ac74
Filip <filip.trifunovic23@gmail.com>
2022-10-20 22:47:36 +0200

    routing pages, navbar, cartbuttons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Navbar, Sidebar, Footer } from "./components";
+
+import {
+  Home,
+  Products,
+  SingleProduct,
+  About,
+  Cart,
+  Error,
+  Checkout,
+  PrivateRoute,
+} from "./pages";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Sidebar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="products" element={<Products />} />
+        <Route path="products/:id" element={<SingleProduct />} />
+        <Route path="checkout" element={<Checkout />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
