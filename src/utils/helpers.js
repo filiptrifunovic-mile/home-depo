477e862672dd9dffa666a8e7cf5665a4ad2a5448
Filip <filip.trifunovic23@gmail.com>
2022-10-21 20:52:36 +0200

    function for getting unique values

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -7,4 +7,8 @@ export const formatPrice = (number) => {
   return newNumber;
 };
 
-export const getUniqueValues = () => {};
+export const getUniqueValues = (data, type) => {
+  let unique = data.flatMap((item) => item[type]);
+
+  return ["all", ...new Set(unique)];
+};
