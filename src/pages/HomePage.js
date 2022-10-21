eec16cb92328a877926744f5baaf4db7d5962822
Filip <filip.trifunovic23@gmail.com>
2022-10-21 13:54:56 +0200

    hero component, services component, contacts component

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,14 @@
 import React from "react";
-
-function HomePage() {
-  return <div>HomePage</div>;
-}
+import { FeaturedProducts, Hero, Services, Contact } from "../components";
+const HomePage = () => {
+  return (
+    <main>
+      <Hero />
+      <FeaturedProducts />
+      <Services />
+      <Contact />
+    </main>
+  );
+};
 
 export default HomePage;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -23,19 +23,19 @@ export const services = [
     id: 1,
     icon: <GiCompass />,
     title: "mission",
-    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi",
+    text: "Serve consumers through online and physical stores and focus on selection, price, and convenience.",
   },
   {
     id: 2,
     icon: <GiDiamondHard />,
     title: "vision",
-    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi",
+    text: "Our vision is to be Earth's most customer-centric company, where customers can find and discover anything they might want to buy online, and endeavours to offer its customers the lowest possible prices.",
   },
   {
     id: 3,
     icon: <GiStabbedNote />,
     title: "history",
-    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi",
+    text: "We are at service of out customers since 2005, and hope to continue for doing our job for many years to come.",
   },
 ];
 
