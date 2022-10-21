cc2f416c37ab65be766d5668909cea275a55b3bc
Filip <filip.trifunovic23@gmail.com>
2022-10-21 19:38:25 +0200

    displaying products in grid view

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { useFilterContext } from "../context/filter_context";
+import GridView from "./GridView";
+import ListView from "./ListView";
 
-function ProductList() {
-  return <div>ProductList</div>;
-}
+const ProductList = () => {
+  const { filtered_products: products } = useFilterContext();
+
+  return <GridView products={products}>product list</GridView>;
+};
 
 export default ProductList;
