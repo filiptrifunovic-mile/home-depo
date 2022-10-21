2e4a0349fd9a1e7989e84fa44b5334fc81d4ac5d
Filip <filip.trifunovic23@gmail.com>
2022-10-21 15:11:44 +0200

    display images

--- src/components/ProductImages.js
+++ src/components/ProductImages.js
@@ -1,7 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
 
-function ProductImages() {
-  return <div>ProductImages</div>;
-}
+const ProductImages = ({ images = [] }) => {
+  const [main, setMain] = useState(images[0]);
+
+  return (
+    <Wrapper>
+      <img src={main?.url} alt="main image" className="main" />
+      <div className="gallery">
+        {images.map((item, index) => {
+          return (
+            <img
+              key={index}
+              src={item.url}
+              onClick={() => setMain(images[index])}
+              className={`${item.url === main.url ? "active" : "null"}`}
+            />
+          );
+        })}
+      </div>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.section`
+  .main {
+    height: 600px;
+  }
+  img {
+    width: 100%;
+    display: block;
+    border-radius: var(--radius);
+    object-fit: cover;
+  }
+  .gallery {
+    margin-top: 1rem;
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    column-gap: 1rem;
+    img {
+      height: 100px;
+      cursor: pointer;
+    }
+  }
+  .active {
+    box-shadow: 0px 0px 0px 2px var(--clr-primary-5);
+  }
+  @media (max-width: 576px) {
+    .main {
+      height: 300px;
+    }
+    .gallery {
+      img {
+        height: 50px;
+      }
+    }
+  }
+  @media (min-width: 992px) {
+    .main {
+      height: 500px;
+    }
+    .gallery {
+      img {
+        height: 75px;
+      }
+    }
+  }
+`;
 
 export default ProductImages;
