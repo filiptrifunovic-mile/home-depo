081057eaa62ca116005fa0a103a28f64be15e048
Filip <filip.trifunovic23@gmail.com>
2022-10-22 14:00:51 +0200

    cart page

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -20,15 +20,27 @@ const CartContext = React.createContext();
 export const CartProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  // add to cart
   function addToCart(id, color, amount, product) {
     dispatch({ type: ADD_TO_CART, payload: { id, color, amount, product } });
   }
 
+  // remover item
+  function removeItem(id) {}
+
+  // toggle amount
+  function toggleAmount(id, value) {}
+
+  //clear cart
+  function clearCart() {}
   return (
     <CartContext.Provider
       value={{
         ...state,
         addToCart,
+        removeItem,
+        toggleAmount,
+        clearCart,
       }}
     >
       {children}
