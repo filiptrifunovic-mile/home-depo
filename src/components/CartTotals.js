9b44032ead1724f52227e9a43ea40d6167d8ae47
Filip <filip.trifunovic23@gmail.com>
2022-10-22 15:22:41 +0200

    condition for checkout if logged in

--- src/components/CartTotals.js
+++ src/components/CartTotals.js
@@ -8,6 +8,8 @@ import { Link } from "react-router-dom";
 const CartTotals = () => {
   const { total_amount, shipping_fee } = useCartContext();
 
+  const { myUser, loginWithRedirect } = useUserContext();
+
   return (
     <Wrapper>
       <div>
@@ -24,9 +26,15 @@ const CartTotals = () => {
             <span>{formatPrice(total_amount + shipping_fee)}</span>
           </h4>
         </article>
-        <Link to="/checkout" className="btn">
-          proceed to checkout
-        </Link>
+        {myUser ? (
+          <Link to="/checkout" className="btn">
+            proceed to checkout
+          </Link>
+        ) : (
+          <button onClick={loginWithRedirect} className="btn" type="button">
+            Login
+          </button>
+        )}
       </div>
     </Wrapper>
   );
