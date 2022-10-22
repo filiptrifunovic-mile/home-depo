6e4674c5cd9842ce3356b4ba89f77d1113578c11
Filip <filip.trifunovic23@gmail.com>
2022-10-22 15:16:22 +0200

    auth0 login/logout functionality

--- src/index.js
+++ src/index.js
@@ -5,14 +5,25 @@ import App from "./App";
 import { ProductsProvider } from "./context/products_context";
 import { FilterProvider } from "./context/filter_context";
 import { CartProvider } from "./context/cart_context";
+import { UserProvider } from "./context/user_context";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>,
+  <Auth0Provider
+    domain="dev-ai4iv8tf.us.auth0.com"
+    clientId="eV4aANuUg7jtgLFrUzmFT8FlLsuKzAOX"
+    redirectUri={window.location.origin}
+    cacheLocation="localstorage"
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>,
   document.getElementById("root")
 );
